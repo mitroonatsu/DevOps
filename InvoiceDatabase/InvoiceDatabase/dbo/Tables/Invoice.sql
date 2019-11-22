b31521a3539b0ed72ba3da0c447441997fc76292
pekkamakkonen <pek.makkonen@gmail.com>
2019-11-22 17:40:32 +0200

    Added database scripts to version control

---
+++ InvoiceDatabase/InvoiceDatabase/dbo/Tables/Invoice.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Invoice (
+	id bigint IDENTITY(1,1),
+	InvoiceSender varchar(50) NOT NULL,
+	RecipientName varchar(50) NOT NULL,
+	RecipientIBAN varchar(18) NOT NULL,
+	Reference varchar(20) NOT NULL,
+	InvoiceNumber varchar(10) NOT NULL,
+	BIC varchar(8) NOT NULL,
+	Total decimal(18,2) NOT NULL,
+	DueDay datetime NOT NULL,
+	Date datetime DEFAULT GETDATE(),
+	PRIMARY KEY (id)
+);
